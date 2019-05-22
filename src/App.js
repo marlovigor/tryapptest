cc2e65c675dda3d9dbbce98fdcfb9c85a69d82bf
marlo <apple@marlo-MacBook-Pro.local>
2019-05-22 02:56:39 -0400

    secondnetlify commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         we did it you soms of bitches
         </a>
       </header>
     </div>
